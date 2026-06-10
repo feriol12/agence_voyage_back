1129e974c99bf078bb7e2cf7707fb9d755dd3b10
Dimas <othentiquedimas@gmail.com>
2026-06-10 16:29:18 +0100

    Protection des routes

--- routes/api.php
+++ routes/api.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinationController;
 
 // Routes accessibles uniquement aux admins
-// Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
-//     Route::apiResource('destinations', DestinationController::class);
-// });
+Route::middleware(['auth:sanctum', 'role:admin'])->prefix('admin')->group(function () {
+    Route::apiResource('destinations', DestinationController::class);
+});
 
 // Routes accessibles à tous (lecture seule)
 Route::get('destinations', [DestinationController::class, 'index']);
 Route::get('destinations/{id}', [DestinationController::class, 'show']);
 
-Route::prefix('admin')->group(function () {
-    Route::apiResource('destinations', DestinationController::class);
-});
+// Route::prefix('admin')->group(function () {
+//     Route::apiResource('destinations', DestinationController::class);
+// });
