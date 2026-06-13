9b73c6a0946defdd897fb02e385b473f15ea143f
feriol12 <gnanviricardo@gmail.com>
2026-06-13 03:40:51 +0100

    crud trips review avec le routes fonctionnelle

--- app/Models/TripDate.php
+++ app/Models/TripDate.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TripDate extends Model
 {
     //
+     use HasFactory;
+ protected $fillable = [
+        'trip_id',
+        'start_date',
+        'end_date',
+        'price',
+        'places_available'
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'price' => 'decimal:2'
+    ];
+
+
+    // Relation avec Trip
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
 }
